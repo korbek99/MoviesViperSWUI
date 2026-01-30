858b2a7a00914ac9846196e992f0c2e3ebece42c
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2026-01-30 18:16:01 -0300

    Initial Commit

---
+++ MoviesViperSWUI/MoviesViperSWUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesViperSWUIApp.swift
+//  MoviesViperSWUI
+//
+//  Created by Jose Preatorian on 30-01-26.
+//
+
+import SwiftUI
+
+@main
+struct MoviesViperSWUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
